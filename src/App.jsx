92bd8cd420f78965c36479bea937e32dd58fdfae
yoonme44 <yoonmem86@gmail.com>
2026-03-28 23:50:17 +0900

    add tailwind frame

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <h1 className="bg-red-300 text-xl">Hello, World!</h1>
       <p>Welcome to my React application.</p>
     </div>
   );
