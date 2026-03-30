5bfc01d0a6a8c5bbcbc2de40555a4d4ba03f6dcb
yoonme44 <yoonmem86@gmail.com>
2026-03-30 23:53:40 +0900

    feat(navbar): add cart and mode switch btn

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Logo from "../../assets/logo.png"
 import { IoMdSearch } from "react-icons/io";
+import { FaCartShopping } from 'react-icons/fa6';
+import DarkMode from './DarkMode';
 
 const Navbar = () => {
   return (
@@ -20,14 +22,22 @@ const Navbar = () => {
                         
                     </a>
                 </div>
-                {/* search bar and cart */}
-                <div>
-                    <div className='group relative hidden sm:block'>
+                <div className='flex justify-between items-center gap-4'>
+                    {/* search bar*/}
+                    <div className='relative group hidden sm:block'>
                         <input type="text" placeholder='search...' 
                         className='w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-200 px-2 py-1 focus:outline-none focus:border-1 focus:border-red-800'/>
-                        <IoMdSearch className='text-gray-500 group-hover:text-primary'/>
+                        <IoMdSearch className='text-gray-500 group-hover:text-primary absolute top-1/2 -translate-y-1/2 right-3'/>
                     </div>
-                    <div></div>
+                    {/* icons */}
+                    <button onClick={() => alert("Ordering is not available yet")} className='bg-gradient-to-r from-primary to-secondary transition-all duration-200 text-white py-1 px-4 rounded-full flex items-center gap-3 group'>
+                        <span className='group-hover:block hidden transition-all duration-200'>Order</span>
+                        <FaCartShopping className='text-xl text-white drop-shadow-sm'/>
+                    </button>
+                    {/* Dark Mode Button */}
+                    <div>
+                        <DarkMode/>
+                    </div>
                 </div>
             </div>
         </div>
